c1cea508346d2987b47c2891ddeb8298e23b5184
EvilEpicCoder <1st.elixile@gmail.com>
2017-10-07 14:39:19 +0300

    first commit

---
+++ EEC_jsTextToggler.js
@@ -0,0 +1,11 @@
+//EvilEpicCoder JavaScript text wrap script
+//add class "info" in to the div and your big text inside will be
+//autoshorted and after click text will unwrap into normal sise
+var jEEC_info={
+"test":"Working!"
+}
+
+window.onload=function(event){
+  var a=new Object(jEEC_info);
+  console.log(a.test);
+}
